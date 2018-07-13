436917ef8795a5b09eb4007ea1931902be279d65
Pasu44 <39987089+Pasu44@users.noreply.github.com>
2018-07-13 16:25:11 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,12 @@
+
+public class reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="aabbcc";
+		StringBuilder sb=new StringBuilder(s);
+		sb.reverse();
+		System.out.println(sb);
+	}
+
+}
